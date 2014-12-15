8ac50ad100624bdd2fb8661508a1daed11fe1920
Sreenivas Kanthamneni <sreenivaskanthamneni@gmail.com>
2014-12-15 00:51:17 -0600

    fixed code

--- fi-services/src/main/java/com/foodizta/api/service/impl/ItemServiceImpl.java
+++ fi-services/src/main/java/com/foodizta/api/service/impl/ItemServiceImpl.java
@@ -1,14 +1,33 @@
 package com.foodizta.api.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.foodizta.api.dao.IAreasTargetedDao;
 import com.foodizta.api.dao.IItemDao;
+import com.foodizta.api.model.ItemDto;
+import com.foodizta.api.model.ResponseDto;
 import com.foodizta.api.service.IItemService;
 
 @Service
 public class ItemServiceImpl implements IItemService {
 
 	@Autowired
-	private IItemDao itemDao;
+	private IAreasTargetedDao areasTargetedDao;
+
+	public ResponseDto getItemsByAreaCode(String zipCode) {
+		ResponseDto responseDto = new ResponseDto();
+		try {
+			List<ItemDto> itemDtoList = areasTargetedDao.getItemsByAreaCode(zipCode);
+			responseDto.setStatus("success");
+			responseDto.setData(itemDtoList);
+		} catch (Exception e) {
+			responseDto.setStatus("error");
+			responseDto.setMessage(e.getMessage());
+		}
+
+		return responseDto;
+	}
 }
