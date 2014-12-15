48b6097f4ef17da53f5932ed6c2f917f2a54a196
Sreenivas Kanthamneni <sreenivaskanthamneni@gmail.com>
2014-12-15 00:54:40 -0600

    added rest controller

--- fi-services/src/main/java/com/foodizta/api/controller/SearchController.java
+++ fi-services/src/main/java/com/foodizta/api/controller/SearchController.java
@@ -1,15 +1,23 @@
 package com.foodizta.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.foodizta.api.model.ResponseDto;
+import com.foodizta.api.service.IItemService;
+
 @RestController
 @RequestMapping("/items/")
 public class SearchController {
+	
+	@Autowired
+	private IItemService itemService;
 
 	@RequestMapping(value = "/{zip}", method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseDto findItemsByZip() {
-		
+	public ResponseDto findItemsByZip(@PathVariable String zip) {
+		return itemService.getItemsByAreaCode(zip);
 	}
 }
